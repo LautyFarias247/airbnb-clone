effae7f4a7af775b21966e9ae9ebee7e8357ac1e
LautyFarías247 <lauty0618@gmail.com>
2023-07-17 14:08:08 -0300

    dynamic searchParams fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import ClientOnly from "@/components/ClientOnly";
+import getListings, { IListingsParams } from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
 import Container from "@/components/Container";
-import EmptyState from "@/components/EmptyState";
-import getListings, { IListingsParams } from "./actions/getListings";
 import ListingCard from "@/components/listings/ListingCard";
-import getCurrentUser from "./actions/getCurrentUser";
-interface Props {
+import EmptyState from "@/components/EmptyState";
+import ClientOnly from "@/components/ClientOnly";
+
+interface HomeProps {
   searchParams: IListingsParams;
 }
-const Home = async ({ searchParams }: Props) => {
+
+const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
+
   if (listings.length === 0) {
     return (
       <ClientOnly>
@@ -17,19 +21,30 @@ const Home = async ({ searchParams }: Props) => {
       </ClientOnly>
     );
   }
+
   return (
     <ClientOnly>
       <Container>
-        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xlgrid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => {
-            return (
-              <ListingCard
-                key={listing.id}
-                data={listing}
-                currentUser={currentUser}
-              />
-            );
-          })}
+        <div
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
